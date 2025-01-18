bb6a5e40276cdb0e0556caffc1dff76fa59bb035
KrisKiselkov <cristian.kiselkov@gmail.com>
2025-01-19 01:13:25 +0200

    Adding React Router & Nav

---
+++ src/components/Nav.js
@@ -0,0 +1,21 @@
+import { NavLink } from "react-router-dom";
+import './Nav.css';
+
+
+export function Nav() {
+    return (
+        <header>
+            <nav>
+                <div className="nav__div nav-div-one">
+                    <NavLink to={"/"} className="nav__a"><p>Продукти</p></NavLink>
+                    <NavLink to={"/"} className="nav__a"><p>Блог</p></NavLink>
+                </div>
+                <div className="nav__div nav-div-two">
+                    <NavLink to={"/"} className="nav__a"><p>Абонаменти</p></NavLink>
+                    <NavLink to={"/"} className="nav__a"><p>За нас</p></NavLink>
+                </div>
+                    <NavLink to={"/"} className="nav-main__a"><p>Hygiene</p></NavLink>
+            </nav>
+        </header>
+    );
+}
