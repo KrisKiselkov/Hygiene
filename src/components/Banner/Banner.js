f6dcd9e4d78562afde28a2e8a8ecdff106199026
KrisKiselkov <cristian.kiselkov@gmail.com>
2025-01-19 23:42:22 +0200

    Banner Complete Look and Func

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,12 +1,39 @@
+import React, { useState, useEffect } from "react";
 import './Banner.css';
 
-
 export function Banner() {
+    const textOptions = [
+        "измий си ръцете.",
+        "измий си зъбите.",
+        "вземи си душ.",
+        "хапни нещо полезно.",
+        "пий повече вода.",
+        "следвай си рутина.",
+        "изгради си хигиенни навици"
+    ];
+
+    const [currentTextIndex, setCurrentTextIndex] = useState(0);
+    const [transitionStage, setTransitionStage] = useState("entering");
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setTransitionStage("exiting");
+            setTimeout(() => {
+                setCurrentTextIndex((prevIndex) => (prevIndex + 1) % textOptions.length);
+                setTransitionStage("entering");
+            }, 500);
+        }, 2500);
+
+        return () => clearInterval(interval);
+    }, []);
+
     return (
         <section className="banner">
             <div className="banner__banner-text">
                 <p className="banner-text__no-change-p">Не бъди странен,</p>
-                <p className="banner-text__change-p">измий си ръцете.</p>
+                <p className={`banner-text__change-p ${transitionStage}`}>
+                    {textOptions[currentTextIndex]}
+                </p>
             </div>
         </section>
     );
