86412a2e1484f932cff9e97b24d80d6bb1fdd608
KrisKiselkov <cristian.kiselkov@gmail.com>
2025-01-22 16:53:24 +0200

    Product Div Base Look

--- src/components/Landing-Main/Main.js
+++ src/components/Landing-Main/Main.js
@@ -42,6 +42,71 @@ export function Main() {
                     </div>
                 </div>
             </div>
+
+            <div className='landing-main__most-bought'>
+                <p className='most-bought__title-p'>Започни промяната към една <span>по-добра</span><br></br> твоя версия чрез <span>нас</span></p>
+
+                <div className='most-bought__mb-products'>
+                    <div className='mb-products__product-div'>
+                        <figure className='product-div__figure'><img src={require('../../images/Aquaphor-Advanced-Therapy-Unscented-Hand-and-Body-Moisturizer-Ointment-for-Cracked-Skin-1-75oz_5f2b8be0-b637-4ed0-bf72-ee2cdca5510e.82f78b82388f236992aa121ae850e46a.png')}></img></figure>
+                    
+                        <button className='product-div__atc-btn'><img src={require('../../images/shopping_cart_24dp_181818_FILL0_wght400_GRAD0_opsz24.png')}></img></button>
+
+                        <div className='product-div__bottom-part'>
+                            <p className='bottom-part__product-label'>Serum Te Teint</p>
+                            <p className='bottom-part__product-price'>$59.00</p>
+                        </div>
+                    </div>
+
+                    <div className='mb-products__product-div'>
+                        <figure className='product-div__figure'><img src={require('../../images/Aquaphor-Advanced-Therapy-Unscented-Hand-and-Body-Moisturizer-Ointment-for-Cracked-Skin-1-75oz_5f2b8be0-b637-4ed0-bf72-ee2cdca5510e.82f78b82388f236992aa121ae850e46a.png')}></img></figure>
+
+                        <button className='product-div__atc-btn'><img src={require('../../images/shopping_cart_24dp_181818_FILL0_wght400_GRAD0_opsz24.png')}></img></button>
+
+                        <div className='product-div__bottom-part'>
+                            <p className='bottom-part__product-label'>Serum Te Teint</p>
+                            <p className='bottom-part__product-price'>$59.00</p>
+                        </div>
+                    </div>
+
+                    <div className='mb-products__product-div'>
+                        <figure className='product-div__figure'><img src={require('../../images/Aquaphor-Advanced-Therapy-Unscented-Hand-and-Body-Moisturizer-Ointment-for-Cracked-Skin-1-75oz_5f2b8be0-b637-4ed0-bf72-ee2cdca5510e.82f78b82388f236992aa121ae850e46a.png')}></img></figure>
+
+                        <button className='product-div__atc-btn'><img src={require('../../images/shopping_cart_24dp_181818_FILL0_wght400_GRAD0_opsz24.png')}></img></button>
+
+                        <div className='product-div__bottom-part'>
+                            <p className='bottom-part__product-label'>Serum Te Teint</p>
+                            <p className='bottom-part__product-price'>$59.00</p>
+                        </div>
+                    </div>
+
+                    <div className='mb-products__product-div'>
+                        <figure className='product-div__figure'><img src={require('../../images/Aquaphor-Advanced-Therapy-Unscented-Hand-and-Body-Moisturizer-Ointment-for-Cracked-Skin-1-75oz_5f2b8be0-b637-4ed0-bf72-ee2cdca5510e.82f78b82388f236992aa121ae850e46a.png')}></img></figure>
+
+                        <button className='product-div__atc-btn'><img src={require('../../images/shopping_cart_24dp_181818_FILL0_wght400_GRAD0_opsz24.png')}></img></button>
+
+                        <div className='product-div__bottom-part'>
+                            <p className='bottom-part__product-label'>Serum Te Teint</p>
+                            <p className='bottom-part__product-price'>$59.00</p>
+                        </div>
+                    </div>
+
+                    <div className='mb-products__product-div'>
+                        <figure className='product-div__figure'><img src={require('../../images/Aquaphor-Advanced-Therapy-Unscented-Hand-and-Body-Moisturizer-Ointment-for-Cracked-Skin-1-75oz_5f2b8be0-b637-4ed0-bf72-ee2cdca5510e.82f78b82388f236992aa121ae850e46a.png')}></img></figure>
+
+                        <button className='product-div__atc-btn'><img src={require('../../images/shopping_cart_24dp_181818_FILL0_wght400_GRAD0_opsz24.png')}></img></button>
+
+                        <div className='product-div__bottom-part'>
+                            <p className='bottom-part__product-label'>Serum Te Teint</p>
+                            <p className='bottom-part__product-price'>$59.00</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className='landing-main__product-range'>
+                
+            </div>
         </main>
     );
 }
