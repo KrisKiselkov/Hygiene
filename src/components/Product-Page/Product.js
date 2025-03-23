ed659e6884b0c4f1e7ebc649c9f6a1cb1ffc58fb
KrisKiselkov <cristian.kiselkov@gmail.com>
2025-03-23 18:45:54 +0200

    All Products Section Base Look & Filters Working

--- src/components/Product-Page/Product.js
+++ src/components/Product-Page/Product.js
@@ -2,6 +2,7 @@ import './Product.css';
 import { Nav } from "../Nav/Nav";
 import { useState, useEffect } from 'react';
 import { productsArray } from '../ProductsArray';
+import { Link } from 'react-router-dom';
 
 
 export function Product() {
@@ -49,6 +50,28 @@ export function Product() {
       
         setProducts(filteredProducts);
     }, [category]);
+
+    const allProducts = () => {
+        try {
+            return (
+                products.map((prod, index) => (
+                    <Link to={`/products/${prod.id}`} className='mb-products__product-div all-products-section__aps-product-div' key={index}>
+                        <button className='product-div__atc-btn'><img src={require('../../images/shopping_cart_24dp_181818_FILL0_wght400_GRAD0_opsz24.png')}></img></button>
+                        <figure className='product-div__figure'><img src={prod.image}></img></figure>
+                            
+
+                        <div className='product-div__bottom-part'>
+                            <p className='bottom-part__product-label'>{prod.label}</p>
+                            <p className='bottom-part__product-price'>${prod.price}</p>
+                        </div>
+                    </Link>
+                    
+                ))
+            )
+        } catch(error) {
+            console.log(error);
+        }         
+    }
 
 
     return (
@@ -125,82 +148,78 @@ export function Product() {
                     </div>
                 </div>
 
-                {/* <div className="products-page__grid">
+
+                <hr className='products-page__hr'></hr>
+
+
+                <div className="products-page__grid">
                     <div className="products-page__filter">
                         <div className='products-page__categories-div'>
                             <div className='products-page__filter-categories'>
                                 <h2 className='products-page__col-h2'>Филтри</h2>
                                         
-                                <div className='products-page__all-categories'>
-                                    <label className='category-div'>
-                                        <div className='category__order-setting'>                                                
-                                            <input type='checkbox' onChange={() => toggleCategory("glutenFree")}
-        checked={category.includes("glutenFree")}></input>
-                                            <span className="checkmark"></span>
-                                            <p className='category__label'>Без Глутен</p>
-                                            
-                                        </div>
-                                    </label>
+                                <div className='products-page__pp-all-categories'>
+                                    <div className='pp-all-categories__type-hygiene'>
+                                        <label className='category-div'>
+                                            <div className='category__order-setting'>                                                
+                                                <input type='checkbox' onChange={() => toggleCategory("dental")}
+            checked={category.includes("dental")}></input>
+                                                <span className="checkmark"></span>
+                                                <p className='category__label'>Дентална</p>
+                                                
+                                            </div>
+                                        </label>
 
-                                    <label className='category-div'>
-                                        <div className='category__order-setting'>
-                                            <input type='checkbox' onChange={() => toggleCategory("vegan")}
-        checked={category.includes("vegan")}></input>
-                                            <span class="checkmark"></span>
-                                            <p className='category__label'>Веган</p>
-                                            
-                                        </div>
-        
-                                    </label>
+                                        <label className='category-div'>
+                                            <div className='category__order-setting'>
+                                                <input type='checkbox' onChange={() => toggleCategory("face")}
+            checked={category.includes("face")}></input>
+                                                <span class="checkmark"></span>
+                                                <p className='category__label'>За лице</p>
+                                                
+                                            </div>
+            
+                                        </label>
 
-                                    <label className='category-div'>
-                                        <div className='category__order-setting'>
-                                            <input type='checkbox' onChange={() => toggleCategory("meat")}
-        checked={category.includes("meat")}></input>
-                                            <span class="checkmark"></span>
-                                            <p className='category__label'>Месоядни</p>
-                                            
-                                        </div>
-                                    </label>
+                                        <label className='category-div'>
+                                            <div className='category__order-setting'>
+                                                <input type='checkbox' onChange={() => toggleCategory("body")}
+            checked={category.includes("body")}></input>
+                                                <span class="checkmark"></span>
+                                                <p className='category__label'>За тяло</p>
+                                                
+                                            </div>
+                                        </label>
 
-                                    <label className='category-div'>
-                                        <div className='category__order-setting'>
-                                            <input type='checkbox' onChange={() => toggleCategory("appetizer")}
-        checked={category.includes("appetizer")}></input>
-                                            <span class="checkmark"></span>
-                                            <p className='category__label'>Предястия</p>
-                                            
-                                        </div>
-                                    </label>
+                                        <label className='category-div'>
+                                            <div className='category__order-setting'>
+                                                <input type='checkbox' onChange={() => toggleCategory("feet")}
+            checked={category.includes("feet")}></input>
+                                                <span class="checkmark"></span>
+                                                <p className='category__label'>За ходила</p>
+                                                
+                                            </div>
+                                        </label>
 
-                                    <label className='category-div'>
-                                        <div className='category__order-setting'>
-                                            <input type='checkbox' onChange={() => toggleCategory("main")}
-        checked={category.includes("main")}></input>
-                                            <span class="checkmark"></span>
-                                            <p className='category__label'>Основни</p>
-                                            
-                                        </div>
-                                    </label>
-
-                                    <label className='category-div'>
-                                        <div className='category__order-setting'>
-                                            <input type='checkbox' onChange={() => toggleCategory("dessert")}
-        checked={category.includes("dessert")}></input>
-                                            <span class="checkmark"></span>
-                                            <p className='category__label'>Десерти</p>
-                                            
-                                        </div>
-                                    </label>                                    
+                                        <label className='category-div'>
+                                            <div className='category__order-setting'>
+                                                <input type='checkbox' onChange={() => toggleCategory("accessories")}
+            checked={category.includes("accessories")}></input>
+                                                <span class="checkmark"></span>
+                                                <p className='category__label'>Аксесоари</p>
+                                                
+                                            </div>
+                                        </label>   
+                                    </div>           
                                 </div>
                             </div>
                         </div>
                     </div>
 
-                    <div className="recipes__section">
-                        
+                    <div className="products-page__all-products-section">
+                        {allProducts()}                       
                     </div>
-                </div> */}
+                </div>
             </section>
         </>
     );
--- src/components/ProductsArray.js
+++ src/components/ProductsArray.js
@@ -49,7 +49,7 @@ export const productsArray = [
         image: require('../images/Aquaphor-Advanced-Therapy-Unscented-Hand-and-Body-Moisturizer-Ointment-for-Cracked-Skin-1-75oz_5f2b8be0-b637-4ed0-bf72-ee2cdca5510e.82f78b82388f236992aa121ae850e46a.png'),
         label: 'Serum Te Teint',
         price: 59.98,
-        type: ["dental"]
+        type: ["accessories"]
     },
     {
         id: '8',
