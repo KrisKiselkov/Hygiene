726c2a6abf628115bd65e88aacbac0e6adaea262
Kristian Kiselkov <Kristian>
2025-05-10 22:15:19 +0300

    Adding Tablet x Mobile filter products menu

--- src/components/Product-Page/Product.js
+++ src/components/Product-Page/Product.js
@@ -31,6 +31,14 @@ export function Product() {
     }, []);
 
     const [ category, setCategory ] = useState([]);
+    const [ catChecked, setCatChecked ] = useState({ 
+        dental: false,
+        face: false,
+        body: false,
+        feet: false, 
+        accessories: false 
+    });
+    const [ mobileFilter, setMobileFilter ] = useState(false);
     const [ products, setProducts ] = useState(productsArray);
 
     const toggleCategory = (filter) => {
@@ -39,6 +47,13 @@ export function Product() {
             ? prevFilters.filter((f) => f !== filter) // Remove filter if already active
             : [...prevFilters, filter] // Add filter if not active
         );
+    };
+
+    const handleCatChecked = (categoryName, isChecked) => {
+        setCatChecked(prevState => ({
+            ...prevState,
+            [categoryName]: isChecked,
+        }));
     };
 
     useEffect(() => {
@@ -51,6 +66,15 @@ export function Product() {
       
         setProducts(filteredProducts);
     }, [category]);
+
+    const openMobileFilter = () => {
+        setMobileFilter(prev => {
+            const newState = !prev;
+            document.body.classList.toggle('no-scroll', newState);
+            return newState;
+        });
+    };
+    
 
     const allProducts = () => {
         try {
@@ -155,12 +179,15 @@ export function Product() {
                     </div>
                 </div>
 
-
                 <hr className='products-page__hr'></hr>
-
 
                 <div className='products-page__pp-filter-products'>
                     <div className='pp-filter-products__pp-filter-section'>
+                        <button className='pp-filter-products__pp-mobile-filter' onClick={openMobileFilter}>
+                            <figure className='pp-mobile-filter__figure'><img src={require('../../images/filter_alt_26dp_181818_FILL0_wght400_GRAD0_opsz24.png')}></img></figure>
+                            Филтри
+                        </button>
+
                         <label className='pp-filter-section__pp-filter-label'>
                             <div className={category.includes("dental") ? 'pp-filter-label__pp-filter-div-true' : 'pp-filter-label__pp-filter-div'}>
                                 <input type='checkbox' className='pp-filter-div__input' onChange={() => toggleCategory("dental")}
@@ -227,6 +254,66 @@ export function Product() {
                     </div>
                 </div>
             </section>
+
+            
+            <div className={`${mobileFilter ? 'product-page__pp-mobile-filter-popup' : 'product-page__pp-mobile-filter-popup--hidden'}`}>
+                <div className='pp-mobile-filter-popup__mobile-filter-section'>
+                    <div className='mobile-filter-section__mobile-filter-header'>
+                        <h2 className='mobile-filter-header__h2'>Избери тип продукт.</h2>
+                        <span className='mobile-filter-header__mobile-close' onClick={openMobileFilter}><figure className='mobile-close__figure'><img src={require('../../images/close_26dp_181818_FILL0_wght400_GRAD0_opsz24.png')}></img></figure></span>
+                    </div>
+
+                    <div className='mobile-filter-section__mobile-filter-main'>
+                        <label className='mobile-filter-main__mobile-filter-label'>
+                            <input type='checkbox' className='mobile-filter-label__input' onChange={(e) => {
+                                toggleCategory("dental");
+                                handleCatChecked("dental", e.target.checked);
+                            }}
+                            checked={category.includes("dental")}>
+                            </input>
+                            <span className='checkmark'></span>
+                            <p className='mobile-filter-label__p'>Дентална</p>
+                        </label>
+
+                        <label className='mobile-filter-main__mobile-filter-label'>
+                            <input type='checkbox' className='mobile-filter-label__input' onChange={(e) => {
+                                toggleCategory("face");
+                                handleCatChecked("face", e.target.checked);
+                            }}
+                            checked={category.includes("face")}>
+                            </input>
+                            <span className='checkmark'></span>
+                            <p className='mobile-filter-label__p'>За лице</p>
+                        </label>
+
+                        <label className='mobile-filter-main__mobile-filter-label'>
+                            <input type='checkbox' className='mobile-filter-label__input' onChange={(e) => {
+                                toggleCategory("body");
+                                handleCatChecked("body", e.target.checked);
+                            }}
+                            checked={category.includes("body")}>
+                            </input>
+                            <span className='checkmark'></span>
+                            <p className='mobile-filter-label__p'>За тяло</p>
+                        </label>
+
+                        <label className='mobile-filter-main__mobile-filter-label'>
+                            <input type='checkbox' className='mobile-filter-label__input' onChange={(e) => {
+                                toggleCategory("accessories");
+                                handleCatChecked("accessories", e.target.checked);
+                            }}
+                            checked={category.includes("accessories")}>
+                            </input>
+                            <span className='checkmark'></span>
+                            <p className='mobile-filter-label__p'>За ходила</p>
+                        </label>
+                    </div>
+
+                    <div className='mobile-filter-section__mobile-filter-footer'>
+
+                    </div>
+                </div>
+            </div>
 
             <Footer />
         </>
