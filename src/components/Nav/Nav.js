15735509abada2d26afc936c26f21a3da1bd3d4b
Kristian Kiselkov <Kristian>
2025-06-23 21:21:48 +0300

    Nav .css / .js Complete

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -35,12 +35,12 @@ export function Nav() {
     return (
         <header>
             <nav>
-                <div className="nav__div nav-div-one">
+                <div className="nav__div">
                     <NavLink to={"/products"} className="nav__a"><p>Продукти</p></NavLink>
                     <NavLink to={"/blog"} className="nav__a"><p>Блог</p></NavLink>
-                </div>
+                
                 <NavLink to={"/"} className="nav-main__a"><p>Hygiene</p></NavLink>
-                <div className="nav__div nav-div-two">
+                
                     <NavLink to={"/subscriptions"} className="nav__a"><p>Абонаменти</p></NavLink>
                     <NavLink to={"/about-us"} className="nav__a"><p>За нас</p></NavLink>
                 </div>
